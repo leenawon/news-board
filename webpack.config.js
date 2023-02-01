cd93ec24c1d55991c1be9f4e28efc3d01b73dc08
leenawon <nawon5154@gmail.com>
2023-02-01 09:59:13 +0900

    Exercise: css 및 이미지 파일들을 자바스크립트로 변환하기위해 webpack 설정 파일에 Loader 적용

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
     path: path.resolve(process.cwd(), "dist"),
     filename: "[name].[contenthash].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: ["file-loader"],
+      },
+    ],
+  },
 };
